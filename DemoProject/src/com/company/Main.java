e345f08f248c4559a6d08d5d771929d5b22ddef6
Yavor Petkov <yavorpetkov92@gmail.com>
2022-02-22 20:19:07 +0200

    Lecture1

---
+++ DemoProject/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+// This is my Main class
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Yavor");
+    }
+}
