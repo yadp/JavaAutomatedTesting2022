4e1d9b2b92451bf591e9ea4002e6b605d95ef23e
Yavor Petkov <yavorpetkov92@gmail.com>
2022-04-12 19:43:20 +0300

    Lecture 12 Countries Set Example

---
+++ Lecture12/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        List<String> listCountries = new LinkedList<>();
+        System.out.println("List add germany"+ listCountries.add("Germany"));
+        listCountries.add("Spain");
+
+        Set<String> countries= new HashSet<>();
+        System.out.println("Set add germany "+countries.add("Germany"));
+        countries.add("Spain");
+
+        System.out.println("Print List"+ listCountries);
+        System.out.println("Print Set"+ countries);
+
+        System.out.println("List add germany"+listCountries.add("Germany"));
+
+        System.out.println("Set add germany "+countries.add("Germany"));
+
+        System.out.println("Print List"+ listCountries);
+        System.out.println("Print Set"+ countries);
+
+        System.out.println(countries.size());
+
+        listCountries.add("Argentina");
+        countries.add("Argentina");
+
+        System.out.println("Print List"+ listCountries);
+        System.out.println("Print Set"+ countries);
+
+    }
+}
