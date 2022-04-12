411fa4ab8b3d4116dc05bba7375f4cc5751a7d66
Yavor Petkov <yavorpetkov92@gmail.com>
2022-04-12 13:51:16 +0300

    Lecture 11 Find Person with min age

--- Lecture11/src/com/company/MainPerson.java
+++ Lecture11/src/com/company/MainPerson.java
@@ -1,24 +1,47 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class MainPerson {
     public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         ArrayList<Person> personList = new ArrayList<>();
         personList.add(new Person(20,"Ted"));
         personList.add(new Person(25,"Bob"));
         personList.add(new Person(30, "Jack"));
         System.out.println(personList);
 
+        Person personWithMinAge=personList.get(0);
         int sumAge=0;
         for (int i = 0; i < personList.size(); i++) {
 
             Person onePerson = personList.get(i);
+            if( onePerson.getAge()< personWithMinAge.getAge()){
+                personWithMinAge = onePerson;
+            }
+
             System.out.println(onePerson);
             int onePersonAge = onePerson.getAge();
             sumAge+=onePersonAge;
             System.out.println("Current sum age is "+ sumAge);
         }
+
+        System.out.println("Person with min age is "+ personWithMinAge);
 
 
         System.out.println("Avg age is "+ sumAge/ personList.size());
@@ -29,5 +52,22 @@ public class MainPerson {
 
         }
 
+
+        System.out.println("Method min age "+ findPersonWithMinAge(personList));
+
+    }
+
+
+    public static Person findPersonWithMinAge(Collection<Person> persons){
+        Person personMinAge= null;
+        for (Person onePerson:persons) {
+            if (personMinAge==null){
+                personMinAge=onePerson;
+            }
+            if( onePerson.getAge()< personMinAge.getAge()){
+                personMinAge=onePerson;
+            }
+        }
+        return personMinAge;
     }
 }
